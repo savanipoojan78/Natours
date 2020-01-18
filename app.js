bb6cff6e4643eb0b42589960e74a50f580e43ed4
Poojan Savani <savanipoojan78@gmail.com>
2020-01-18 15:09:17 +0530

    Update Api endpoint aded

--- app.js
+++ app.js
@@ -61,6 +61,21 @@ app.post('/api/v1/tours', (req, res) => {
     );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid Id'
+        });
+    }
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour: 'Update Sucessfull'
+        }
+    });
+});
+
 const port = 9033;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
