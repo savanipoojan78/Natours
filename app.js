39c7f13ad9019215ad42fdb9fd99cf9168736eb4
Poojan Savani <savanipoojan78@gmail.com>
2020-01-18 14:14:34 +0530

    API handling GET Request v1/tours

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200);
-    res.json({ message: 'Hello from the server Side', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//     res.status(200);
+//     res.json({ message: 'Hello from the server Side', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-    res.send('ou can post to this URL');
-})
+// app.post('/', (req, res) => {
+//     res.send('ou can post to this URL');
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+});
 
 const port = 9033;
 app.listen(port, () => {
