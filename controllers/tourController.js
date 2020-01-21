3a5c5ae7e3040d701bc8b6b96838cfc1669c0c32
Poojan Savani <savanipoojan78@gmail.com>
2020-01-21 14:04:19 +0530

    Paging Done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,40 +2,62 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async(req, res) => {
     try {
-        //BUILD QUERY
-        //1A) Filtering
-        const queryObj = {...req.query };
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach(el => delete queryObj[el]);
-
-        //console.log(req.query, queryObj);
-
-        //1B) Advance Filtering
-        // { difficulty: 'easy', duration: { $gte: '5' } }
-        // { difficulty: 'easy', duration: { gte: '5' } }
-
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        //console.log(JSON.parse(queryStr));
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        //2) Sorting
-        console.log(req.query);
-
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-            //
-        } else {
-            query = query.sort('-createdAt');
-        }
-
         // const query = await Tour.find()
         //     .where('duration')
         //     .equals(5)
         //     .where('difficulty')
         //     .equals('easy');
+
+        //BUILD QUERY
+
+        //1A) Filtering
+        const queryObj = {...req.query };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach(el => delete queryObj[el]);
+
+
+        //1B) Advance Filtering
+
+        // { difficulty: 'easy', duration: { gte: '5' } } // Convert this req response to the given below
+        // { difficulty: 'easy', duration: { $gte: '5' } }
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        let query = Tour.find(JSON.parse(queryStr));
+
+        //2) Sorting
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            //By default sorting by createdAt in asc order
+            query = query.sort('-createdAt');
+        }
+
+        //3) Fileds limiting
+
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields); //Projecting
+        } else {
+            //here - indicate that that field is excluded.
+            query = query.select('-__v');
+        }
+
+        //Paging
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 100;
+        const skip = (page - 1) * limit;
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) {
+                throw new Error("This Page Doesn't exit ");
+            }
+        }
+
+        query = query.skip(skip).limit(limit);
 
         // EXECUTE THE QUERY
         const tours = await query;
