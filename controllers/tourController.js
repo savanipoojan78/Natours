4c940ece2e9e9585a42476f17b809d23225843ad
Poojan Savani <savanipoojan78@gmail.com>
2020-01-21 14:55:13 +0530

    Refactoring API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFetures');
 
 exports.aliasTopTours = async(req, res, next) => {
     req.query.limit = '5';
@@ -6,66 +7,18 @@ exports.aliasTopTours = async(req, res, next) => {
     req.query.fields = 'name,price,ratingAverage,difficulty';
     next();
 };
+
 exports.getAllTours = async(req, res) => {
     try {
-        // const query = await Tour.find()
-        //     .where('duration')
-        //     .equals(5)
-        //     .where('difficulty')
-        //     .equals('easy');
-
         //BUILD QUERY
-
-        //1A) Filtering
-        const queryObj = {...req.query };
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach(el => delete queryObj[el]);
-
-        //1B) Advance Filtering
-
-        // { difficulty: 'easy', duration: { gte: '5' } } // Convert this req response to the given below
-        // { difficulty: 'easy', duration: { $gte: '5' } }
-
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        //2) Sorting
-
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        } else {
-            //By default sorting by createdAt in asc order
-            query = query.sort('-createdAt');
-        }
-
-        //3) Fileds limiting
-
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields); //Projecting
-        } else {
-            //here - indicate that that field is excluded.
-            query = query.select('-__v');
-        }
-
-        //Paging
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 100;
-        const skip = (page - 1) * limit;
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) {
-                throw new Error("This Page Doesn't exit ");
-            }
-        }
-
-        query = query.skip(skip).limit(limit);
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
 
         // EXECUTE THE QUERY
-        const tours = await query;
+        const tours = await features.query;
 
         //SEND RESPONSE
         res.status(200).json({
---
+++ utils/apiFetures.js
@@ -0,0 +1,60 @@
+class APIFeatures {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    filter() {
+        const queryObj = {...this.queryString.query };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach(el => delete queryObj[el]);
+
+        //1B) Advance Filtering
+
+        // { difficulty: 'easy', duration: { gte: '5' } } // Convert this req response to the given below
+        // { difficulty: 'easy', duration: { $gte: '5' } }
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    sort() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            this.query.sort(sortBy);
+        } else {
+            //By default sorting by createdAt in asc order
+            this.query.sort('-createdAt');
+        }
+        return this;
+    }
+
+    limitFields() {
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(',').join(' ');
+            this.query.select(fields); //Projecting
+        } else {
+            //here - indicate that that field is excluded.
+            this.query.select('-__v');
+        }
+        return this;
+    }
+
+    paginate() {
+        const page = parseInt(this.queryString.page) || 1;
+        const limit = parseInt(this.queryString.limit) || 100;
+        const skip = (page - 1) * limit;
+        this.query.skip(skip).limit(limit);
+        /**
+         * we are returing this keword in ever function beause we went chaining
+         * like filter().sort().paginate().   so that wh we are returing this keword
+         * so that it returns the object and we can access the quer and querString in another methode
+         *
+         **/
+        return this;
+    }
+}
+module.exports = APIFeatures;
