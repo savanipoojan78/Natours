80b737004abe6919e3c55723dc00767704c38357
Poojan Savani <savanipoojan78@gmail.com>
2020-01-23 15:11:35 +0530

    Aggregate mongoose part-1

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -107,3 +107,40 @@ exports.deleteTour = async(req, res) => {
         });
     }
 };
+
+exports.getTourStats = async(req, res) => {
+    try {
+        const stats = await Tour.aggregate([{
+                $match: { ratingsAverage: { $gte: 4.5 } }
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    // _id: '$ratingsAverage',
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    numTours: { $sum: 1 },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' }
+                }
+            },
+            {
+                $sort: { avgPrice: 1 }
+            },
+            // {
+            //     $match: { _id: { $ne: 'EASY' } }
+            // }
+            
+        ]);
+        res.status(200).json({
+            status: 'Success',
+            stats
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'Failed',
+            message: error
+        });
+    }
+};
