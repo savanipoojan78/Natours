a647d67294a36425fa32a56f12b13bf4ff8a8807
Poojan Savani <poojan@noonacademy.com>
2020-04-15 19:57:22 +0530

    changes in .env variable

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config({ path: '../../config.env' });
 const Tour = require('../../models/tourModel');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-);
-
+const DB=process.env.DATABASE_LOCAL;
 mongoose
     .connect(DB, {
         useNewUrlParser: true,
