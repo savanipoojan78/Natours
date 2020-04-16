3ffd37f081d0145d0f5c6eac46fc3cd75ed3d479
Poojan Savani <poojan@noonacademy.com>
2020-04-16 14:40:55 +0530

    added router handler

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-//Create Our Own MiddleWare
-app.use((req, res, next) => {
-    console.log('Hello from the Middleware');
-    next();
-});
-
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
@@ -32,5 +26,13 @@ app.use((req, res, next) => {
 //this two are middleware , Mount Our Router // if we get Router like '/api/v1/tours' then go to this @tourRouter Function
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*',(req,res,next)=>{
+    res.status(400).json({
+        status:'failed',
+        message:`route ${req.originalUrl} is not defined`
+    })
+    next();
+});
 
 module.exports = app;
