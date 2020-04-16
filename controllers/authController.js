9dcec47be4a26914acf608e8c27ba7b3237c4634
Poojan Savani <poojan@noonacademy.com>
2020-04-17 02:07:15 +0530

    added Jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,15 @@
 const User=require('./../models/userModel');
 const catchAsync=require('./../utils/catchAsync');
+const jwt=require('jsonwebtoken')
 
 exports.signup=catchAsync(async (req,res)=>{
     const newUser=await User.create(req.body);
+    const token=jwt.sign({id:newUser._id},process.env.JWT_SECRECT,{
+        expiresIn:process.env.JWT_EXPIRE_IN
+    })
     res.status(201).json({
         status:'success',
+        token,
         data:{
             user:newUser
         }
