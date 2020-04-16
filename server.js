eb624a6bdc515fd4878c7d27f7c8649b41053045
Poojan Savani <poojan@noonacademy.com>
2020-04-17 00:28:37 +0530

    uncaughtException

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException',err=>{
+    console.log(err.name,err.message);
+    process.exit(1);
+})
+
 const app = require('./app');
 
 //console.log(process.env);
