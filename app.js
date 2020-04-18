b9d3797eaddeaefcb3db543feee4b61f91fe4254
Poojan Savani <poojan@noonacademy.com>
2020-04-18 16:29:00 +0530

    added parameter pollution

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const helmet=require('helmet');
 const mongoSanitize=require('express-mongo-sanitize');
 const xss=require('xss-clean')
+const hpp=require('hpp')
 const rateLimit=require('express-rate-limit')
 const AppError=require('./utils/appError')
 const tourRouter = require('./routes/tourRoutes');
@@ -41,6 +42,18 @@ app.use(mongoSanitize())
 
 //Data sanitization agaist XSS
 app.use(xss())
+
+//Prevent Parameter Pollution
+app.use(hpp({
+    whitelist:[
+        'ratingsAverage',
+        'ratingsQuantity',
+        'duration',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}))
 
 // Test Middleware
 app.use((req, res, next) => {
