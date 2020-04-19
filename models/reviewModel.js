8d71128344965b4c344a91f45752419fab2a9c17
Poojan Savani <poojan@noonacademy.com>
2020-04-19 15:35:44 +0530

    added avg rating and nRating

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Tour=require('./tourModel');
 const reviewSchema=new mongoose.Schema({
     review:{
         type:String,
@@ -29,7 +29,30 @@ const reviewSchema=new mongoose.Schema({
     toJSON:{virtuals:true},
     toObject:{virtuals:true}
 });
+reviewSchema.statics.calculateAvgRating=async function(tourId){
+    console.log(tourId);
+    const stats=await this.aggregate([
+        {
+            $match:{tour:tourId}
+        },
+        {
+            $group:{
+                _id:'$tour',
+                nRating:{$sum:1},
+                avgRating:{$avg:'$rating'}
+            }
+        }
+    ]);
+    console.log(stats);
+    await Tour.findByIdAndUpdate(tourId,{
+        ratingsQuantity:stats[0].nRating,
+        ratingsAverage:stats[0].avgRating
+    })
+}
 
+reviewSchema.post('save',function(){
+    this.constructor.calculateAvgRating(this.tour)
+})
 reviewSchema.pre(/^find/,function(next){
     // this.populate({
     //     path:'tour',
@@ -44,5 +67,7 @@ reviewSchema.pre(/^find/,function(next){
     })
     next();
 })
+
+
 const Review=mongoose.model('Reivew',reviewSchema);
 module.exports=Review;
